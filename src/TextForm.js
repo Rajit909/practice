6ebc665600943d6c6bb14e1086832a6e2a835c83
Rajitkumar <krajit909@gmail.com>
2023-02-07 21:59:06 +0530

    updated

--- src/TextForm.js
+++ src/TextForm.js
@@ -7,10 +7,12 @@ function TextForm(props) {
     let newText = text.toLocaleUpperCase();
     setText(newText)
   }
+  
   const handleDownClick = ()=>{
     let newText = text.toLocaleLowerCase()
     setText(newText)
   }
+
   const handleClearClick = ()=>{
     setText("")
   }
@@ -19,7 +21,20 @@ function TextForm(props) {
   const handleOnChange = (event)=>{
     setText(event.target.value)
   }
-  const [ text, setText] = useState("Enter text here")
+
+  const handleCopy = () => {
+    console.log("Copied text")
+    var text = document.querySelector("#myBox")
+    text.select();
+    navigator.clipboard.writeText(text.value)
+  }
+
+  const handleExtraSpace = () => {
+    let newText = text.split(/[ ]+/)
+    setText(newText.join(" "))
+  }
+
+  const [ text, setText] = useState("")
 
   return (
     <>
@@ -27,10 +42,13 @@ function TextForm(props) {
     <div className='container'>
     <h1 className='text-center m-4' style={{color:props.mode === 'dark'? '#fff':'black'}}>Text Converter</h1>
     <div className='container col-8'>
-    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode === 'dark'? 'gray':'white', color:props.mode === 'dark'? '#fff':'black'}} id="exampleFormControlTextarea1" rows="8"></textarea>
+    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode === 'dark'? 'gray':'white', color:props.mode === 'dark'? '#fff':'black'}} 
+    id="myBox" rows="8"></textarea>
     <button className='btn btn-primary mt-4 mx-2' onClick={handleUpClick}>Converter to uppercase</button>
     <button className='btn btn-primary mt-4 mx-2' onClick={handleDownClick}>Converter to lowercase</button>
     <button className='btn btn-primary mt-4 mx-2' onClick={handleClearClick}>Clear Text</button>
+    <button className='btn btn-primary mt-4 mx-2' onClick={handleCopy}>Copy Text</button>
+    <button className='btn btn-primary mt-4 mx-2' onClick={handleExtraSpace}>Remove space</button>
     </div>
     </div>
     <div className="container my-5">
