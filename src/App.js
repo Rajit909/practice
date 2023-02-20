5f05b6276429616fe30eac27081f14cdb0b37474
Rajitkumar <krajit909@gmail.com>
2023-02-20 21:40:20 +0530

    router used

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import TextForm from './TextForm';
 import Navbar from './Navbar';
-// import About from './About';
+import About from './About';
 import { useState } from 'react';
 import Alert from './Alert.js';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
   const [mode, setMode] = useState('light')
@@ -27,6 +32,12 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
+      setInterval(() => {
+        document.title="Install TextUtils now"
+      }, 1500);
+      setInterval(() => {
+        document.title = 'TextUtils- Dark Mode'
+      }, 3000);
     }else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
@@ -36,12 +47,21 @@ function App() {
 
   return (
     <>
+    <Router>
+
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className='container my-3'>
-    <TextForm showAlert={showAlert} mode={mode}/>
+    <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <TextForm showAlert={showAlert} mode={mode}/>
+          </Route>
+    </Switch>
     </div>
-    {/* <About/> */}
+    </Router>
     </>
   );
 }
